a5ea9190da6f51f4900d10c9084699a908bdd795
boring_nick <boring-nick@users.noreply.github.com>
2022-09-06 19:50:18 +0300

    feat: !rustlog join and part commands

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,22 @@
 use anyhow::Context;
 use serde::{Deserialize, Serialize};
+use std::{collections::HashSet, sync::RwLock};
 use tokio::fs;
+use tracing::info;
 
 const CONFIG_FILE_NAME: &str = "config.json";
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Config {
     pub logs_directory: String,
     #[serde(default = "default_listen_address")]
     pub listen_address: String,
-    pub channels: Vec<String>,
+    pub channels: RwLock<HashSet<String>>,
     #[serde(rename = "clientID")]
     pub client_id: String,
     pub client_secret: String,
+    pub admins: Vec<String>,
 }
 
 impl Config {
@@ -24,12 +27,13 @@ impl Config {
         serde_json::from_str(&contents).context("Config deserializtion error")
     }
 
-    /*pub async fn save(&self) -> anyhow::Result<()> {
+    pub async fn save(&self) -> anyhow::Result<()> {
+        info!("Updating config");
         let json = serde_json::to_string_pretty(self)?;
         fs::write(CONFIG_FILE_NAME, &json).await?;
 
         Ok(())
-    }*/
+    }
 }
 
 fn default_listen_address() -> String {
