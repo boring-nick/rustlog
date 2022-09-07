c771847b0ec50a7e291225fe650abfb920fe4328
boring_nick <boring-nick@users.noreply.github.com>
2022-09-07 20:22:42 +0300

    feat: make reindex sync

--- src/reindexer.rs
+++ src/reindexer.rs
@@ -11,12 +11,14 @@ use futures::future::try_join_all;
 use std::{
     collections::{hash_map::Entry, HashMap},
     convert::TryFrom,
+    io::{BufRead, BufWriter, Seek, Write},
     time::Instant,
 };
-use tokio::{
+use std::{
     fs::{self, File, OpenOptions},
-    io::{AsyncBufReadExt, AsyncSeekExt, AsyncWriteExt, BufReader, BufWriter},
+    io::BufReader,
 };
+use tokio::task::spawn_blocking;
 use tracing::{debug, error, info, trace, warn};
 use twitch_irc::message::{IRCMessage, ServerMessage};
 
@@ -58,9 +60,7 @@ pub async fn reindex_channel(
 
             if users_path.exists() {
                 info!("Clearing existing indexes in channel {channel_id} for {year}-{month}");
-                fs::remove_dir_all(&users_path)
-                    .await
-                    .context("could not clear old indexes")?;
+                fs::remove_dir_all(&users_path).context("could not clear old indexes")?;
             }
 
             for day in days {
@@ -75,9 +75,7 @@ pub async fn reindex_channel(
                     false,
                 );
 
-                let file = File::open(channel_file_path)
-                    .await
-                    .context("could not open channel file")?;
+                let file = File::open(channel_file_path).context("could not open channel file")?;
                 let mut reader = BufReader::new(file);
                 let mut offset;
 
@@ -86,38 +84,39 @@ pub async fn reindex_channel(
                 let mut user_writers = HashMap::new();
 
                 loop {
-                    offset = reader.stream_position().await?;
-                    let read_bytes = reader.read_line(&mut line).await?;
+                    offset = reader.stream_position()?;
+                    let read_bytes = reader.read_line(&mut line)?;
                     if read_bytes == 0 {
                         break;
                     }
 
                     match IRCMessage::parse(line.trim_end()) {
                         Ok(irc_message) => {
-                            let maybe_user_id = match ServerMessage::try_from(irc_message.clone()) {
-                                Ok(server_msg) => {
-                                    if let Some((_, Some(user))) =
-                                        extract_channel_and_user(&server_msg)
-                                    {
-                                        Some(match user {
-                                            UserIdentifier::UserId(id) => id.to_owned(),
-                                            UserIdentifier::User(name) => {
-                                                app.get_user_id_by_name(name).await?
+                            let maybe_user_id =
+                                if let Some(user_id) = irc_message.tags.0.get("user-id") {
+                                    user_id.clone()
+                                } else {
+                                    match ServerMessage::try_from(irc_message) {
+                                        Ok(server_msg) => {
+                                            if let Some((_, Some(user))) =
+                                                extract_channel_and_user(&server_msg)
+                                            {
+                                                Some(match user {
+                                                    UserIdentifier::UserId(id) => id.to_owned(),
+                                                    UserIdentifier::User(name) => {
+                                                        app.get_user_id_by_name(name).await?
+                                                    }
+                                                })
+                                            } else {
+                                                None
                                             }
-                                        })
-                                    } else {
-                                        None
+                                        }
+                                        Err(err) => {
+                                            debug!("{err}");
+                                            None
+                                        }
                                     }
-                                }
-                                Err(err) => {
-                                    debug!("{err}, attempting basic extraction");
-                                    if let Some(user_id) = irc_message.tags.0.get("user-id") {
-                                        user_id.clone()
-                                    } else {
-                                        None
-                                    }
-                                }
-                            };
+                                };
 
                             if let Some(user_id) = maybe_user_id {
                                 let index = Index {
@@ -138,7 +137,7 @@ pub async fn reindex_channel(
                                             &month.to_string(),
                                         );
                                         if !users_folder.exists() {
-                                            fs::create_dir_all(&users_folder).await?;
+                                            fs::create_dir_all(&users_folder)?;
                                         }
 
                                         let user_file_path =
@@ -148,8 +147,7 @@ pub async fn reindex_channel(
                                         let user_file = OpenOptions::new()
                                             .create(true)
                                             .append(true)
-                                            .open(user_file_path)
-                                            .await?;
+                                            .open(user_file_path)?;
                                         let writer = BufWriter::new(user_file);
 
                                         vacant.insert(writer)
@@ -158,22 +156,20 @@ pub async fn reindex_channel(
 
                                 user_writer
                                     .write_all(&index.as_bytes())
-                                    .await
                                     .context("Could not write to user log")?;
                             } else {
-                                debug!("Skipping message {irc_message:?}");
+                                debug!("Skipping message {line:?}");
                             }
-
-                            line.clear();
                         }
                         Err(err) => warn!("Malfromed message: {line} {err}"),
                     }
+                    line.clear();
                 }
 
                 try_join_all(
                     user_writers
                         .into_values()
-                        .map(|mut writer| tokio::spawn(async move { writer.flush().await })),
+                        .map(|mut writer| spawn_blocking(move || writer.flush())),
                 )
                 .await
                 .context("Could not flush users")?;
