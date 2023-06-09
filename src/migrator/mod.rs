8a94c1e57cb6815c7818d148ba5df6a2160efa16
boring_nick <boring-nick@users.noreply.github.com>
2023-06-09 17:43:11 +0300

    make migrator slightly faster

--- src/migrator/mod.rs
+++ src/migrator/mod.rs
@@ -192,7 +192,7 @@ async fn write_line<'a>(
     inserter: &mut Inserter<Message<'_>>,
     datetime: DateTime<Utc>,
 ) -> anyhow::Result<()> {
-    match twitch::Message::parse(raw) {
+    match twitch::Message::parse_with_whitelist(raw, twitch::whitelist!(TmiSentTs, UserId)) {
         Some(irc_message) => {
             let timestamp = extract_raw_timestamp(&irc_message)
                 .unwrap_or_else(|| datetime.timestamp_millis() as u64);
