a046a381c56f66db56a03b35ae1dbe81f7780d14
boring_nick <boring-nick@users.noreply.github.com>
2024-03-01 17:24:53 +0200

    Add ability to disable colours in logs

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use futures::{future::try_join_all, stream::FuturesUnordered, StreamExt};
 use migrator::Migrator;
 use mimalloc::MiMalloc;
 use std::{
+    env,
     sync::Arc,
     time::{Duration, Instant},
 };
@@ -46,10 +47,15 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let use_ansi = env::var("RUST_LOG_ANSI")
+        .ok()
+        .and_then(|ansi| ansi.parse().ok())
+        .unwrap_or(true);
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
         )
+        .with_ansi(use_ansi)
         .init();
 
     let config = Config::load()?;
