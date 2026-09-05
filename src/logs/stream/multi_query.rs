05088cbee1e634456da3bb3f12aaf98fcc31541b
ByteZ1337 <42820438+ByteZ1337@users.noreply.github.com>
2026-09-05 18:53:17 +0200

    Merge commit from fork

--- src/logs/stream/multi_query.rs
+++ src/logs/stream/multi_query.rs
@@ -41,67 +41,69 @@ impl Stream for MultiQueryStream {
     type Item = Result<Vec<StructuredMessage<'static>>>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        if let Some(buffer_response) = self.buffer_response.take() {
-            if buffer_response.is_at_start() {
-                if !buffer_response.messages.is_empty() {
-                    self.count += buffer_response.len();
-                    return Poll::Ready(Some(Ok(buffer_response.messages)));
+        loop {
+            if let Some(buffer_response) = self.buffer_response.take() {
+                if buffer_response.is_at_start() {
+                    if !buffer_response.messages.is_empty() {
+                        self.count += buffer_response.len();
+                        return Poll::Ready(Some(Ok(buffer_response.messages)));
+                    }
+                } else {
+                    // Put it back if not used
+                    self.buffer_response = Some(buffer_response);
                 }
-            } else {
-                // Put it back if not used
-                self.buffer_response = Some(buffer_response);
             }
-        }
 
-        if let Some(limit) = self.limit {
-            if self.count >= limit {
-                return Poll::Ready(None);
+            if let Some(limit) = self.limit {
+                if self.count >= limit {
+                    return Poll::Ready(None);
+                }
             }
-        }
 
-        let current = self.current;
-        match self.cursors.get_mut(current) {
-            Some(cursor) => {
-                let next_line_poll = {
-                    let fut = cursor.next();
-                    pin!(fut);
-                    fut.poll(cx)
-                };
-
-                match next_line_poll {
-                    Poll::Ready(Ok(Some(msg))) => {
-                        if let Some(offset) = self.offset {
-                            if self.count < offset {
-                                return self.poll_next(cx);
+            let current = self.current;
+            match self.cursors.get_mut(current) {
+                Some(cursor) => {
+                    let next_line_poll = {
+                        let fut = cursor.next();
+                        pin!(fut);
+                        fut.poll(cx)
+                    };
+                    match next_line_poll {
+                        Poll::Ready(Ok(Some(msg))) => {
+                            if let Some(offset) = self.offset {
+                                if self.count < offset {
+                                    self.count += 1;
+                                    continue;
+                                }
                             }
-                        }
 
-                        self.count += 1;
-                        Poll::Ready(Some(Ok(vec![msg])))
+                            self.count += 1;
+                            return Poll::Ready(Some(Ok(vec![msg])));
+                        }
+                        Poll::Ready(Err(err)) => return Poll::Ready(Some(Err(err.into()))),
+                        Poll::Ready(Ok(None)) => {
+                            self.current += 1;
+                            continue;
+                        }
+                        Poll::Pending => return Poll::Pending,
                     }
-                    Poll::Ready(Err(err)) => Poll::Ready(Some(Err(err.into()))),
-                    Poll::Ready(Ok(None)) => {
-                        self.current += 1;
-                        self.poll_next(cx)
-                    }
-                    Poll::Pending => Poll::Pending,
                 }
-            }
-            None => {
-                let response = self
-                    .buffer_response
-                    .take()
-                    .filter(|buffer| !buffer.is_at_start() && !buffer.is_empty())
-                    .map(|buffer| match self.limit {
-                        Some(limit) => buffer
-                            .messages
-                            .into_iter()
-                            .take(limit.saturating_sub(self.count))
-                            .collect(),
-                        None => buffer.messages,
-                    })
-                    .map(Ok);
-                Poll::Ready(response)
+                None => {
+                    let response = self
+                        .buffer_response
+                        .take()
+                        .filter(|buffer| !buffer.is_at_start() && !buffer.is_empty())
+                        .map(|buffer| match self.limit {
+                            Some(limit) => buffer
+                                .messages
+                                .into_iter()
+                                .take(limit.saturating_sub(self.count))
+                                .collect(),
+                            None => buffer.messages,
+                        })
+                        .map(Ok);
+                    return Poll::Ready(response);
+                }
             }
         }
     }
